ef2410923d76f165cd022bb9e6dc233e5ab33703
javiergonzalez02 <javi.galeon2012@gmail.com>
2023-10-26 11:14:12 +0200

    Nuevo constructor

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -23,6 +23,12 @@ public class  MaquinaExpendedoraSimple {
         estacionOrigen = "León";
         estacionDestino = "Madrid";
     }
+    
+    public MaquinaExpendedoraSimple(int precioDelBillete, String origen, String destino) {
+        precioBillete = precioDelBillete;
+        estacionOrigen = "León";
+        estacionDestino = destino;
+    }
 
     /**
      * Devuelve el precio del billete
