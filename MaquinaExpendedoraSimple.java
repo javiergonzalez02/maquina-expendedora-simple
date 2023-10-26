c69ebbd516959f57062941f1dba84d430045e30f
javiergonzalez02 <javi.galeon2012@gmail.com>
2023-10-26 12:32:04 +0200

    Get dinero acumulado

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -44,6 +44,10 @@ public class  MaquinaExpendedoraSimple {
     public int getBalanceClienteActual() {
         return balanceClienteActual;
     }
+    
+    public int getTotalDineroAcumulado (){
+        return totalDineroAcumulado;
+    }
 
     /**
      * Simula la introduccion de dinero por parte del cliente actual
